c58fc42e1ec05d6d46ffed2c9d7790b73bd9350d
Bo Oo <bhoo@iastate.edu>
2024-08-25 17:10:20 -0500

    second body page configured

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -88,14 +88,61 @@ function Body() {
 //this will show the requirement for an international student
 function Secondbody() {
   return (
-    <main className="bg-gray-500 p-20  flex-grow">
-      {/* This will  */}
+    <main className="bg-blue-300 p-20  flex-grow">
+      {/* This body is for English proficiency requirement*/}
       <div>
         <h1 className="flex justify-start text-3xl text-white">
           English proficiency requirement
         </h1>
+        <div className="mt-5">
+          {/* GPA requirement */}
+          <div>
+            <h1 className="text-white">GPA Requirement</h1>
+            <p className="mt-2">
+              Equivalent of a minimum 3.0 high school GPA on 4.0 scale. We
+              understands many different evaluation systems are used throughout
+              the world, Colleges and University are usually the one to
+              determine equivalent GPA.
+            </p>
+          </div>
+
+          {/* Course Requirement  */}
+          <div className="mt-5">
+            <h1 className="text-white">Course Requirement</h1>
+            <p className="mt-2">
+              In addition to the requirement above, students must meet the
+              following minimum high school course requirements:
+            </p>
+            <ul className="mt-2 ml-3">
+              <li>4 years of English or your native language</li>
+              <li>
+                3 years of science (including at least two years of laboratory
+                science courses which emphasize elements of biology, chemistry,
+                or physics)
+              </li>
+              <li>3 years of math (including algebra and geometry)</li>
+              <li>2 years of social studies</li>
+            </ul>
+          </div>
+
+          {/* SAT/ACT Score Requirement  */}
+          <div className="mt-5">
+            <h1 className="text-white">SAT/ACT Score Requirement</h1>
+            <p>
+              While SAT or ACT scores are not required for admission, students
+              who achieve either of the two minimum test scores below are
+              automatically considered to have met the 3.00 GPA requirement:
+            </p>
+            <ul className="mt-2 ml-3">
+              <li>
+                1200 SAT combined score (Evidence Based Reading and Writing
+                score + Math score)
+              </li>
+              <li>25 ACT composite score</li>
+            </ul>
+          </div>
+        </div>
       </div>
-      
     </main>
   );
 }
