eccd010cb5e457ab54a67fef02b4a37dd30a06a0
Bo Oo <bhoo@iastate.edu>
2024-08-25 20:38:20 -0500

    header, footer for second page

--- frontend/src/AboutUs.js
+++ frontend/src/AboutUs.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-function Mainpage() {
+function MainHeader() {
   return <div></div>;
 }
 
+function MainPage() {
+  return <div className="flex-grow"></div>;
+}
+
+function Footer() {
+  return (
+    <footer className="bg-white-800 text-black p-4 mt-auto text-center ">
+      <p>&copy; 2024 ACCO. All rights reserved.</p>
+    </footer>
+  );
+}
+
 function AboutUs() {
-  return <Mainpage />;
+  return (
+    <div>
+      <MainHeader />
+      <MainPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default AboutUs;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
-import HomePage from './HomePage';
-import Login from './Login';
-import AboutUs from './AboutUs';
+import "./App.css";
+import HomePage from "./HomePage";
+import Login from "./Login";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
-    <HomePage />
+    // <HomePage />,
+    <AboutUs />
   );
 }
 
